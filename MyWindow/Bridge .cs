0514670af9b3e96418021121c3ea0ecc98f1712b
245597377 <245597377@qq.com>
2017-12-26 14:25:40 +0800

    2017年12月26日14:25:24
    2017年12月26日14:25:21

---
+++ MyWindow/Bridge .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MyWindow
+{
+    class Bridge
+    {
+        [DllImport(@"MyDll.dll", EntryPoint = "SetForegroundWindowEx, CallingConvention=CallingConvention.Cdecl")]
+        extern public static bool SetForegroundWindowEx(char[] chr);
+
+        [DllImport(@"MyDll.dll", EntryPoint = "GetWindowRectEx", CallingConvention = CallingConvention.Cdecl)]
+        extern public static bool GetWindowRectEx();
+
+        [DllImport(@"MyDll.dll", EntryPoint = "SetCursorPosEx", CallingConvention = CallingConvention.Cdecl)]
+        extern public static bool SetCursorPosEx(int x, int y);
+
+        [DllImport(@"MyDll.dll", EntryPoint = "FindWindowWEx", CallingConvention = CallingConvention.Cdecl)]
+        extern public static bool FindWindowWEx(char[] chr);
+
+        [DllImport(@"MyDll.dll", EntryPoint = "InputEx", CallingConvention = CallingConvention.Cdecl)]
+        extern public static void InputEx(int key);
+
+        [DllImport(@"MyDll.dll", EntryPoint = "ClickEx", CallingConvention = CallingConvention.Cdecl)]
+        extern public static void ClickEx();
+    }
+}
